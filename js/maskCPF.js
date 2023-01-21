46008923b115981e1446ff405033ccc3f18e9361
Samuel Rocha <samuel.rocarvalho@gmail.com>
2023-01-21 00:47:50 -0300

    commit inicial

---
+++ js/maskCPF.js
@@ -0,0 +1,21 @@
+function mascara(i){
+   
+    var v = i.value;
+    
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+    
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+ 
+ }
+
+ function mCEP(cep){
+    cep=cep.replace(/\D/g,"")
+    cep=cep.replace(/^(\d{2})(\d)/,"$1.$2")
+    cep=cep.replace(/\.(\d{3})(\d)/,".$1-$2")
+    return cep
+}
